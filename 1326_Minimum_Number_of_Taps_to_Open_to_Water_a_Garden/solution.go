a46aca89614ceb294c2553b5ecd596af00d36a3a
HRay <hray3182@gmail.com>
2023-12-20 21:57:56 +0800

    Refactor minTaps function and add tests

--- 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution.go
+++ 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution.go
@@ -2,69 +2,77 @@
 
 package leetcode
 
-import "sort"
-
 // https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
 
 // 需要一個2維矩陣記錄灑水器的範圍
 //
-func MinTaps(n int, ranges []int) int {
-	taps := make([][]int, n+1)
-	for i := range taps {
-		taps[i] = make([]int, 2)
-		// 記錄灑水器範圍
-		taps[i][0] = max(0, i-ranges[i]) // 左邊界
-		taps[i][1] = min(n, i+ranges[i]) // 右邊界
+func minTaps(n int, ranges []int) int {
+	// Find the reach of each position, NOT each tap. We do this by finding the left
+	// and right ends of each tap, and then creating a list with the reach from the
+	// left, aka pretending like that's where the water stream starts, and is only
+	// shooting in a straight line.
+	reach := make([]int, n+1)
+	for i := 0; i < len(ranges); i++ {
+		left := max(0, i-ranges[i])
+		right := min(n, i+ranges[i])
+		reach[left] = max(reach[left], right)
 	}
-	// 排序矩陣
-	// 規則如下:
-	// 1. 左邊界小的在前面
-	// 2. 左邊界相同的, 右邊界大的在前面
-	sort.Slice(taps, func(i, j int) bool {
-		if taps[i][0] == taps[j][0] {
-			return taps[i][1] > taps[j][1]
-		}
-		return taps[i][0] < taps[j][0]
-	})
-	// 檢查第一個灑水器有沒有覆蓋到最左邊(0)
-	if taps[0][0] > 0 {
-		return -1
 
-	}
-	// 記錄右邊界
-	right := taps[0][1]
-	// 記錄水龍頭數量
-	count := 0
-
-	// 由於已經排序好的關係，所以可以直接遍歷，從最小的左邊界開始
-	for i := range taps {
-		// 更新完 right 後灑水器數量 + 1
-		count++
-		// 如果 right >= n 的話，表示已經覆蓋整個花園，此時就可以結束
-		if right >= n {
-			return count
-		}
-		currRight := right
-		for j := i + 1; j < len(taps); j++ {
-			// 如果 j 的覆蓋範圍比 i 還大的話，則將 i 取代為 j
-			if taps[j][0] <= right && taps[j][1] >= currRight {
-				i = j
-				currRight = taps[j][1]
-			}
-		}
-		// 如果 j 遍歷完無法超過一開始記錄的 right 的話，則代表中間有斷層，
-		// 接下來無論如何都無法覆蓋整個花園
-		// 只需要關注是否等於，小於的話 currRight 不會被更新
-		if currRight == right {
+	// Count how many taps it takes to get us to the max reach of all water streams.
+	globalMax := reach[0]
+	localMax := reach[0]
+	taps := 1
+	for i := 0; i < n; i++ {
+		// If we've ever started checking a point that's farther than the farthest
+		// reach that we know about, that means no water stream has been able to
+		// reach this point yet, and no stream ever will (because we're only looking
+		// at streams shooting in a straight line.)
+		if i > globalMax {
 			return -1
 		}
-		// 將右邊界更新成目前的水龍頭的右邊界
-		right = currRight
 
-		// 由於 i 會自增，所以需要減 1，用當前找到最大範圍的 i 來做為下一次循環的開始
-		i--
+		// Check if the reach from this position is farther than the absolute
+		// maximum that we know about, and update if it is.
+		globalMax = max(globalMax, reach[i])
+
+		// If we've reached a position that's farther than the "current" maximum
+		// that we're tracking, that means we need to be tracking a new water stream,
+		// so we add 1 to our tap count, and set the local maximum that we're tracking
+		// to the absolute maximum that we know about.
+		if i == localMax {
+			localMax = globalMax
+			taps++
+		}
 	}
-	return -1
+	return taps
+}
+
+// 從別人的解答看到的，超級優雅解法
+func minTaps2(n int, ranges []int) int {
+	reach := make([]int, n+1)
+	for i := range ranges {
+		// 用 left 當作 index, right 做為value
+		left := max(0, i-ranges[i])
+		right := min(n, i+ranges[i])
+		reach[left] = max(reach[left], right)
+	}
+
+	if n > 0 && reach[0] == 0 {
+		return -1
+	}
+	globalMax, localMax := reach[0], reach[0]
+	count := 1
+	for i := 1; i <= n; i++ {
+		if i > globalMax {
+			return -1
+		}
+		if i > localMax {
+			localMax = globalMax
+			count++
+		}
+		globalMax = max(globalMax, reach[i])
+	}
+	return count
 }
 
 func max(a, b int) int {
--- 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution_test.go
+++ 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution_test.go
@@ -4,23 +4,49 @@ import "testing"
 
 func TestMinTaps(t *testing.T) {
 	var result int
-	result = MinTaps(5, []int{3, 4, 1, 1, 0, 0})
+	result = minTaps(5, []int{3, 0, 1, 1, 0, 0})
 	if result != 1 {
 		t.Errorf("MinTaps failed, got %d, expected 1", result)
 	}
-	result = MinTaps(3, []int{0, 0, 0, 0})
+	result = minTaps(3, []int{0, 0, 0, 0})
 	if result != -1 {
 		t.Errorf("MinTaps failed, got %d, expected -1", result)
 	}
-	result = MinTaps(7, []int{1, 2, 1, 0, 2, 1, 0, 1})
+	result = minTaps(7, []int{1, 2, 1, 0, 2, 1, 0, 1})
 	if result != 3 {
 		t.Errorf("MinTaps failed, got %d, expected 3", result)
 	}
-	result = MinTaps(8, []int{4, 0, 0, 0, 0, 0, 0, 0, 4})
+	result = minTaps(8, []int{4, 0, 0, 0, 0, 0, 0, 0, 4})
 	if result != 2 {
 		t.Errorf("MinTaps failed, got %d, expected 2", result)
 	}
-	result = MinTaps(8, []int{4, 0, 0, 0, 4, 0, 0, 0, 4})
+	result = minTaps(8, []int{4, 0, 0, 0, 4, 0, 0, 0, 4})
+	if result != 1 {
+		t.Errorf("MinTaps failed, got %d, expected 1", result)
+	}
+}
+
+func TestMinTaps2(t *testing.T) {
+	var result int
+	result = minTaps2(5, []int{3, 0, 1, 1, 0, 0})
+	// [[0,3],[1,1],[1,3],[2,4],[4,4],[5,5]]
+
+	if result != -1 {
+		t.Errorf("MinTaps failed, got %d, expected 1", result)
+	}
+	result = minTaps2(3, []int{0, 0, 0, 0})
+	if result != -1 {
+		t.Errorf("MinTaps failed, got %d, expected -1", result)
+	}
+	result = minTaps2(7, []int{1, 2, 1, 0, 2, 1, 0, 1})
+	if result != 3 {
+		t.Errorf("MinTaps failed, got %d, expected 3", result)
+	}
+	result = minTaps2(8, []int{4, 0, 0, 0, 0, 0, 0, 0, 4})
+	if result != 2 {
+		t.Errorf("MinTaps failed, got %d, expected 2", result)
+	}
+	result = minTaps2(8, []int{4, 0, 0, 0, 4, 0, 0, 0, 4})
 	if result != 1 {
 		t.Errorf("MinTaps failed, got %d, expected 1", result)
 	}
