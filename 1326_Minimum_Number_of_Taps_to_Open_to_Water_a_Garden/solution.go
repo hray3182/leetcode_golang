f184ce7f289aea8b02c70908527df8a76bf2fd0d
HRay <hray3182@gmail.com>
2023-12-20 22:08:47 +0800

    update

--- 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution.go
+++ 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution.go
@@ -2,49 +2,65 @@
 
 package leetcode
 
+import "sort"
+
 // https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
 
 // 需要一個2維矩陣記錄灑水器的範圍
 //
 func minTaps(n int, ranges []int) int {
-	// Find the reach of each position, NOT each tap. We do this by finding the left
-	// and right ends of each tap, and then creating a list with the reach from the
-	// left, aka pretending like that's where the water stream starts, and is only
-	// shooting in a straight line.
-	reach := make([]int, n+1)
-	for i := 0; i < len(ranges); i++ {
-		left := max(0, i-ranges[i])
-		right := min(n, i+ranges[i])
-		reach[left] = max(reach[left], right)
+	taps := make([][]int, n+1)
+	for i := range taps {
+		taps[i] = make([]int, 2)
+		// 記錄灑水器範圍
+		taps[i][0] = max(0, i-ranges[i]) // 左邊界
+		taps[i][1] = min(n, i+ranges[i]) // 右邊界
 	}
+	// 排序矩陣
+	// 規則如下:
+	// 1. 左邊界小的在前面
+	// 2. 左邊界相同的, 右邊界大的在前面
+	sort.Slice(taps, func(i, j int) bool {
+		if taps[i][0] == taps[j][0] {
+			return taps[i][1] > taps[j][1]
+		}
+		return taps[i][0] < taps[j][0]
+	})
+	// 檢查第一個灑水器有沒有覆蓋到最左邊(0)
+	if taps[0][0] > 0 {
+		return -1
 
-	// Count how many taps it takes to get us to the max reach of all water streams.
-	globalMax := reach[0]
-	localMax := reach[0]
-	taps := 1
-	for i := 0; i < n; i++ {
-		// If we've ever started checking a point that's farther than the farthest
-		// reach that we know about, that means no water stream has been able to
-		// reach this point yet, and no stream ever will (because we're only looking
-		// at streams shooting in a straight line.)
-		if i > globalMax {
+	}
+	// 記錄右邊界
+	right := taps[0][1]
+	// 記錄水龍頭數量
+	count := 0
+
+	for i := range taps {
+		count++
+		// 如果 right >= n 的話，表示已經覆蓋整個花園，此時就可以結束
+		if right >= n {
+			return count
+		}
+		currRight := right
+		for j := i + 1; j < len(taps); j++ {
+			// 如果 j 的覆蓋範圍比 i 還大的話，則將 i 取代為 j
+			if taps[j][0] <= right && taps[j][1] >= currRight {
+				i = j
+				currRight = taps[j][1]
+			}
+		}
+		// 如果 j 遍歷完沒有超過一開始記錄的 right 的話，則代表一開始的範圍已經是最大的了
+		// 接下來無論如何都無法覆蓋整個花園
+		if currRight == right {
 			return -1
 		}
+		right = currRight
 
-		// Check if the reach from this position is farther than the absolute
-		// maximum that we know about, and update if it is.
-		globalMax = max(globalMax, reach[i])
-
-		// If we've reached a position that's farther than the "current" maximum
-		// that we're tracking, that means we need to be tracking a new water stream,
-		// so we add 1 to our tap count, and set the local maximum that we're tracking
-		// to the absolute maximum that we know about.
-		if i == localMax {
-			localMax = globalMax
-			taps++
-		}
+		// 由於 i 會自增，所以需要減 1，用當前找到最大範圍的 i 來做為下一次循環的開始
+		i--
 	}
-	return taps
+	return -1
 }
 
 // 從別人的解答看到的，超級優雅解法
